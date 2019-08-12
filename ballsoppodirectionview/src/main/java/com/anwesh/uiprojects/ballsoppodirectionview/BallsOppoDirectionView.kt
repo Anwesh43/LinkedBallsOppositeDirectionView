b68492e7f167cefe32c33cf9a22e123de596b321
Anwesh43 <anweshthecool0@gmail.com>
2019-08-12 17:07:28 +0530

    adding libraries and constants

--- ballsoppodirectionview/src/main/java/com/anwesh/uiprojects/ballsoppodirectionview/BallsOppoDirectionView.kt
+++ ballsoppodirectionview/src/main/java/com/anwesh/uiprojects/ballsoppodirectionview/BallsOppoDirectionView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.ballsoppodirectionview
 /**
  * Created by anweshmishra on 12/08/19.
  */
+
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val balls : Int = 2
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val scGap : Float = 2.9f
+val scDiv : Double = 0.51
+val backColor : Int = Color.parseColor("#BDBDBD")
