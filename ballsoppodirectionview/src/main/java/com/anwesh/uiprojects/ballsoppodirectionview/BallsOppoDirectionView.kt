c602da822dbabedc8b2ab97a92f87209770c7713
Anwesh43 <anweshthecool0@gmail.com>
2019-08-12 17:19:04 +0530

    adding Canvas extension methods

--- ballsoppodirectionview/src/main/java/com/anwesh/uiprojects/ballsoppodirectionview/BallsOppoDirectionView.kt
+++ ballsoppodirectionview/src/main/java/com/anwesh/uiprojects/ballsoppodirectionview/BallsOppoDirectionView.kt
@@ -20,6 +20,7 @@ val foreColor : Int = Color.parseColor("#0D47A1")
 val scGap : Float = 2.9f
 val scDiv : Double = 0.51
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 2.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -30,3 +31,34 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawOppositeBalls(i : Int, h : Float, size : Float, sc : Float, paint : Paint) {
+    val r : Float = size / rFactor
+    save()
+    translate(0f, (h - r) * (1 - sc) * (1 - 2 * i))
+    drawCircle(0f, 0f, r, paint)
+    restore()
+}
+
+fun Canvas.drawBODNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(90f * sc1)
+    drawLine(0f, -size, 0f, size, paint)
+    for (j in 0..1) {
+        save()
+        translate(0f, size * (1f - 2 * j))
+        drawOppositeBalls(i, h / 2 - size, size, sc1, paint)
+        restore()
+    }
+    restore()
+}
