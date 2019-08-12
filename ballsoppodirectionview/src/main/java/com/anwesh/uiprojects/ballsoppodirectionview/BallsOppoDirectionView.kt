faa8d914355b82ce6785a9b6122c0496f43366ab
Anwesh43 <anweshthecool0@gmail.com>
2019-08-12 17:31:45 +0530

    adding BODNode class

--- ballsoppodirectionview/src/main/java/com/anwesh/uiprojects/ballsoppodirectionview/BallsOppoDirectionView.kt
+++ ballsoppodirectionview/src/main/java/com/anwesh/uiprojects/ballsoppodirectionview/BallsOppoDirectionView.kt
@@ -125,4 +125,48 @@ class BallsOppoDirectionView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BODNode(var i : Int, val state : State = State()) {
+
+        private var next : BODNode? = null
+        private var prev : BODNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BODNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBODNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BODNode {
+            var curr : BODNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
